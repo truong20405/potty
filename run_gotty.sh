2e52bbec70cca7b3f0c8eeee079625d7575373d3
truong20405 <164866193+truong20405@users.noreply.github.com>
2025-01-07 18:27:08 +0700

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Kiểm tra xem tmux session đã tồn tại chưa
+tmux has-session -t mysession 2>/dev/null
 
-/usr/local/bin/gotty --permit-write --reconnect /bin/bash
+# Nếu session chưa tồn tại, tạo mới
+if [ $? != 0 ]; then
+    tmux new-session -d -s mysession
+fi
+
+# Kết nối với tmux session
+tmux attach -t mysession
